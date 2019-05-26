1131af4ab8222e02697192f6e07ea10010ca8e79
charan7386 <gummadavallicharansai12@gmail.com>
2019-05-26 12:56:23 +0530

    Updates

---
+++ precache-manifest.cdb80c904a1e7d1b851538c1428c6e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa68f505bb6950e88760d202ea78584",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "bd84618aa5c871c63afc",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8eac7c6631907a33eab5",
+    "url": "/contactmanager/static/css/main.a3e82a03.chunk.css"
+  },
+  {
+    "revision": "bd84618aa5c871c63afc",
+    "url": "/contactmanager/static/js/2.46c1028d.chunk.js"
+  },
+  {
+    "revision": "8eac7c6631907a33eab5",
+    "url": "/contactmanager/static/js/main.11981645.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
